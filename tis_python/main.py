6d191d37825fcd3686cae96933ca6f50e5af8cc1
Jiabei Zhu <zjb@bu.edu>
2022-07-22 12:40:17 -0400

    a simple test for sequential capture

--- tis_python/main.py
+++ tis_python/main.py
@@ -2,15 +2,26 @@ import serial
 import ctypes
 import tisgrabber as tis
 import numpy as np
+import logging
+import time
 
-ic = tis.loadLib()
+logging.basicConfig(format='%(asctime)s [%(levelname).1s] %(message)s', level=logging.DEBUG)
 
-hGrabber = tis.openDevice(ic)
-arduino = serial.Serial('COM3', timeout=1)  # 1s timeout
+EXPOSURE = 0.5
+
+def soft_trigger():
+    raise NotImplementedError
+
+def capture_callback(_, pBuffer, framenumber, pData):
+    print(pData.BytesPerPixel, pData.height)
+    image = np.ndarray(buffer=pBuffer.contents,
+                       dtype=np.uint16,
+                       shape=(pData.height.value, pData.width.value))
+    print(image.shape)
 
 
-def capture(ic):
-    if ic.IC_SnapImage(hGrabber, 2000) == tis.IC_SUCCESS:
+def capture(ic, grabber, save=None):
+    if ic.IC_SnapImage(grabber, 2000) == tis.IC_SUCCESS:
         # Declare variables of image description
         Width = ctypes.c_long()
         Height = ctypes.c_long()
@@ -18,49 +29,72 @@ def capture(ic):
         colorformat = ctypes.c_int()
 
         # Query the values of image description
-        ic.IC_GetImageDescription(hGrabber, Width, Height,
+        ic.IC_GetImageDescription(grabber, Width, Height,
                                   BitsPerPixel, colorformat)
-        print(Width.value, Height.value, BitsPerPixel.value, colorformat.value)
 
         # Calculate the buffer size
-        bpp = int(BitsPerPixel.value / 8.0)
+        assert BitsPerPixel.value == 16
+        assert colorformat.value == tis.SinkFormats.Y16
         buffer_size = Width.value * Height.value * BitsPerPixel.value
 
         # Get the image data
-        imagePtr = ic.IC_GetImagePtr(hGrabber)
+        imagePtr = ic.IC_GetImagePtr(grabber)
 
-        imagedata = ctypes.cast(imagePtr,
-                                ctypes.POINTER(ctypes.c_ubyte *
-                                               buffer_size))
+        imagedata = ctypes.cast(imagePtr, ctypes.POINTER(ctypes.c_ubyte * buffer_size))
 
         # Create the numpy array
         image = np.ndarray(buffer=imagedata.contents,
-                           dtype=np.uint8,
-                           shape=(Height.value,
-                                  Width.value,
-                                  bpp))
+                           dtype=np.uint16,
+                           shape=(Height.value, Width.value))
+        if save is not None:
+            np.save(f'{save}.npy', image)
+            return True
+        else:
+            return image
     else:
-        print("No frame received in 2 seconds.")
+        logging.warning("No frame received in 2 seconds.")
 
-if not ic.IC_IsDevValid(hGrabber):
-    ic.IC_MsgBox(tis.T("No device opened"), tis.T("Simple Live Video"))
-else:
-    try:
-        ic.IC_StartLive(hGrabber, 1)
-        finish = False
-        arduino.write(b'N')
-        while not finish:
-            out = arduino.readline()
-            if out[:3] == b'[N]':
-                capture(ic)
-                arduino.write(b'N')
-            elif out[:3] == b'[F]':
-                finish = True
-            elif out[:3] == b'[D]':
-                print(out)
-            else:
-                print(f'get invalid content: {out}')
-                finish = True
-    finally:
-        ic.IC_StopLive(hGrabber)
-        ic.IC_ReleaseGrabber(hGrabber)
+
+def main():
+    ic = tis.loadLib()
+    hGrabber = tis.openDevice(ic)
+    arduino = serial.Serial('COM3', timeout=1)  # 1s timeout
+    images = []
+
+    if not ic.IC_IsDevValid(hGrabber):
+        ic.IC_MsgBox(b"No device opened", b"Auto capturing")
+    else:
+        try:
+            ic.IC_SetFormat(hGrabber, tis.SinkFormats.Y16.value)
+            ic.IC_SetPropertySwitch(hGrabber, b"Exposure", b"Auto", 0)
+            ic.IC_SetPropertyAbsoluteValue(hGrabber, b"Exposure", b"Value", ctypes.c_float(EXPOSURE))
+            ic.IC_StartLive(hGrabber, 1)
+            while capture(ic, hGrabber) is None:  # successfully capture -> initialized
+                pass
+            num = 1
+            arduino.write(b'N')
+            while True:
+                out = arduino.readline()
+                logging.debug('raw serial data: %s', out)
+                match out[:3]:
+                    case b'[N]':
+                        logging.debug('capture start %d', num)
+                        images.append(capture(ic, hGrabber))
+                        logging.debug('capture end %d', num)
+                        num += 1
+                        arduino.write(b'N')
+                    case b'[F]':  # finished
+                        return np.stack(images)
+                    case b'[D]':  # debugging info
+                        logging.info('arduino says: %s', out[3:])
+                    case _:
+                        logging.error(f'get invalid content: %s', out)
+        finally:
+            ic.IC_StopLive(hGrabber)
+            ic.IC_ReleaseGrabber(hGrabber)
+            arduino.close()
+            logging.info('finished')
+
+
+if __name__ == '__main__':
+    imgs = main()
--- tis_python/tisgrabber/__init__.py
+++ tis_python/tisgrabber/__init__.py
@@ -1,2 +1,5 @@
 from .tisgrabber import *
 from .loader import loadLib
+from enum import IntEnum
+SinkFormats = IntEnum('SinkFormats', [(e.name, e.value) for e in SinkFormats])
+FRAMEFILTER_PARAM_TYPE = IntEnum('FRAMEFILTER_PARAM_TYPE', [(e.name, e.value) for e in FRAMEFILTER_PARAM_TYPE])
